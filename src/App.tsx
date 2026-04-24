2facb242b8fdc9b51e2bcf9b878347b74748e705
Devanshu Tripathi <74350750+DevanshuTripathi@users.noreply.github.com>
2026-04-24 15:39:27 +0530

    Remove comment from App.tsx
    Removed unnecessary comment from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,8 @@ import './App.css'
 import { UserProvider } from "./hooks/useUser"
 import { Toaster } from "react-hot-toast"
 
+// Pls remove this comment
+
 // ADMIN COMPONENTS
 import AdminRoute from "./pages/components/AdminRoute"
 import AdminAddQuestion from "./pages/AddQuestion"
